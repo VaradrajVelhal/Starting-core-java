0cb7f22395330542868355dd10e62e815e24bfd4
VaradrajVelhal <varadrajvelhal1@gmail.com>
2025-06-30 03:10:32 -0700

    Created generic.java
    I learned about generics......

---
+++ generic.java
@@ -0,0 +1,23 @@
+//example of generic
+class Example<t> {
+    t s;
+   Example(t var1) {
+      this.s = var1;
+   }
+
+   public void getter() {
+      System.out.println(this.s);
+   }
+   public Object getter2(){
+    return s;
+   }
+}
+public class generic {
+    public static void main(String[] args) {
+       Example<Integer> example=new Example<>(2021);
+        example.getter();
+        Example<String> example2=new Example<>("hello");
+        System.out.println(example2.s.getClass().getName());//gives output[java.lang.String]
+
+    }
+}

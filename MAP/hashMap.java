59a1af5119c3c35f81cb0ba1143d5f71f14ed4ca
VaradarajVelhal <varadrajvelhal1@gmail.com>
2025-07-05 03:37:11 -0700

    renamed a folder

---
+++ MAP/hashMap.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class hashMap {
+    public static void main(String[] args) {
+         HashMap<Integer,String> map = new HashMap<>();
+        //this is Syntax for HashMap you have to specify keys data type(here Integer)
+        //and values datatype (here String)
+
+        map.put(1, "Robert");//put is used to put values in HashMap
+        map.put(4, "Ned");
+        map.put(11, "Tywin");
+        map.put(5, "Orthur");
+
+        System.out.println(map);
+
+        String ans=map.get(11);//get() is used to get value on given input here its 11 so it will 
+        //return Tywin
+        System.out.println(ans);
+
+        System.out.println(map.containsKey(34));//it will return false because this 34 key
+        //is not present in hashmap
+
+        System.out.println(map.containsValue("Orthur"));// it checks for value in hashmap
+
+        //to use loop on HashMap we have to use map.keySet()
+        for (int i : map.keySet()) {
+            System.out.println(map.get(i));//it will print values 
+        }
+
+        map.remove(11);//to remove key valuepair
+        
+    }
+}
---
+++ MAP/map.java
@@ -0,0 +1,19 @@
+
+
+public class map {
+ public static void main(String[] args) {
+   //the map is like Dictionary where you look for word to find its definitiom
+   //MAP is an object that maps keys to values.It cannot contain duplicate keys,
+   //and each key can map to at most one value....
+   //map does not extend the collection interface
+
+   //Characteristics
+   //Key value pairs-- Each entry in a map consists of a key and value.
+   //Unique keys -- NO two entries can have the same key.
+   //One value per key
+   //Order -- Some implementations maintain insertion order (Linked Hash Map ) ,natural order
+   //(Tree Map),or no order (Hash Map)....
+
+   
+ }   
+}
